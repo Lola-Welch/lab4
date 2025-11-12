c0bb4a798ee19d1cc23182d2ddad829487bf2d31
thelo <thelolagent20@gmail.com>
2025-11-12 13:59:52 -0800

    Co-authored-by: SkimmyMilk937 <SkimmyMilk937@users.noreply.github.com>

--- bst.py
+++ bst.py
@@ -3,3 +3,37 @@ import unittest
 from typing import *
 from dataclasses import dataclass
 sys.setrecursionlimit(10**6)
+
+BinTree : TypeAlias = Union["Node", None]
+
+@dataclass (frozen = True)
+class Node:
+    elt : Any
+    left : BinTree
+    right : BinTree
+    
+
+def comes_before():
+    p
+
+
+@dataclass (frozen=True)
+class BinarySearchTree(self):
+    elt = comes_before(Self
+    tree : BinTree
+    
+#check this  : why is iit only allowing one argument for a BST?
+# determine whether a BST is empty or not
+def is_empty(bst : BinarySearchTree) -> bool:
+    match bst:
+        case BinarySearchTree(None):
+            return True
+        case BinarySearchTree(_):
+            return False
+
+
+
+
+
+def insert (bst : BinarySearchTree, value : Any) -> BinarySearchTree:
+    
