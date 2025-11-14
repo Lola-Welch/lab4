747f9574f2a7fee0c70a2564c084a890d1ba7532
SkimmyMilk937 <110652867+SkimmyMilk937@users.noreply.github.com>
2025-11-14 14:17:55 -0800

    y7o

--- bst.py
+++ bst.py
@@ -13,20 +13,25 @@ class Node:
     right : BinTree
     
 
-def comes_before():
-    p
+def comes_before(num1 : int, num2 : int) -> bool:
+    return num1 > num2
+
+def comes_before(char1 : str, char2 : str) -> bool:
+    return ord(char1) > ord(char2)
+    
+
 
 
 @dataclass (frozen=True)
-class BinarySearchTree(self):
-    elt = comes_before(Self
+class BinarySearchTree(Self):
+    comes_before : Callable[[Any, Any], bool]
     tree : BinTree
     
 #check this  : why is iit only allowing one argument for a BST?
 # determine whether a BST is empty or not
 def is_empty(bst : BinarySearchTree) -> bool:
     match bst:
-        case BinarySearchTree(None):
+        case None:
             return True
         case BinarySearchTree(_):
             return False
@@ -34,6 +39,23 @@ def is_empty(bst : BinarySearchTree) -> bool:
 
 
 
-
 def insert (bst : BinarySearchTree, value : Any) -> BinarySearchTree:
+
+    bst = BinarySearchTree( bst.comes_before, None) # change
+    insert_recurse(bst.tree, value)
     
+    
+    
+    def insert_recurse(node : BinTree, value : Any) -> BinTree:
+        if(bst.comes_before(value, node.elt)):
+            if(node.left is None):
+                return node
+            return insert_recurse(value, node.left)
+        else:
+            if(node.right is None):
+                return node
+            return insert_recurse(value, node.right)
+        
+    def makeNewTree
+        
+        
